463f469859a4eaba754e4683e1ed5925b0c91c71
kothale3d <kothale3d@gmail.com>
2025-10-04 10:55:22 +0530

    Add backend folder

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json()); 
+
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
